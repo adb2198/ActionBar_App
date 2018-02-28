2c160a919e1acfb9488895fedf1fc837f45272be
bhosleaditya4@gmail.com <bhosleaditya4@gmail.com>
2018-02-28 06:31:06 +0530

    Initial commit

---
+++ app/src/main/java/com/example/aditya/action_bar/Second.java
@@ -0,0 +1,13 @@
+package com.example.aditya.action_bar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Second extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
